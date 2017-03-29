a65779f032a189262bcc3838125cbbbaad4febeb
Pedro Pazello <email@pedropazello.com>
2017-03-28 23:10:50 -0300

    Implementa as mudanças vistas na aula 4

--- src/App.js
+++ src/App.js
@@ -2,22 +2,66 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
+import InputCustomizado from './componentes/InputCustomizado.js';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {lista : []};
+    this.state = { lista: [], nome: '', email: '', password: '' };
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setPassword = this.setPassword.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     $.ajax({
         url:"http://localhost:3001/api/autores",
         dataType: 'json',
         success:function(resposta){
-          this.setState({lista:resposta});
+          this.setState({ lista:resposta });
         }.bind(this)
       }
     );
+  }
+
+  enviaForm(evento) {
+    evento.preventDefault();
+    console.log("dados sendo enviados");
+    $.ajax({
+        url:"http://localhost:3001/api/autores",
+        contentType: 'application/json',
+        dataType:'json',
+        type:'post',
+        data: JSON.stringify({
+          autor: {
+            nome: this.state.nome,
+            email: this.state.email,
+            password: this.state.password
+          }
+        }),
+        success: function(resposta) {
+            console.log("enviado com sucesso");
+            let listaAtual = this.state.lista;
+            listaAtual.push(resposta);
+            this.setState({ lista: listaAtual });
+        }.bind(this),
+        error: function(resposta) {
+            console.log("erro");
+        }
+      });
+  }
+
+  setNome(evento){
+    this.setState({nome: evento.target.value});
+  }
+
+  setEmail(evento){
+    this.setState({email: evento.target.value});
+  }
+
+  setPassword(evento){
+    this.setState({password: evento.target.value});
   }
 
   render() {
@@ -46,19 +90,13 @@ class App extends Component {
             </div>
             <div className="content" id="content">
               <div className="pure-form pure-form-aligned">
-                <form className="pure-form pure-form-aligned">
-                  <div className="pure-control-group">
-                    <label htmlFor="nome">Nome</label>
-                    <input id="nome" type="text" name="nome" value=""  />
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="email">Email</label>
-                    <input id="email" type="email" name="email" value=""  />
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="senha">Senha</label>
-                    <input id="senha" type="password" name="senha"  />
-                  </div>
+                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                  <InputCustomizado id="nome" type="text" name="nome"
+                    value={this.state.nome} onChange={this.setNome} labelFor="nome" label="Nome"  />
+                  <InputCustomizado id="email" type="email" name="email"
+                    value={this.state.email} onChange={this.setEmail} labelFor="email" label="Email"  />
+                  <InputCustomizado id="password" type="password" name="password"
+                      value={this.state.password} onChange={this.setPassword} labelFor="password" label="Senha" />
                   <div className="pure-control-group">
                     <label></label>
                     <button type="submit" className="pure-button pure-button-primary">Gravar</button>
@@ -88,9 +126,7 @@ class App extends Component {
               </div>
             </div>
           </div>
-
-
-</div>
+      </div>
     );
   }
 }
